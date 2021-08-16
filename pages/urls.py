7ed0b7f6598c4c3d7bd1d48519929a096d66877d
Alibouss <bouss58@hotmail.com>
2021-08-16 11:33:52 +0200

    mise en place des template files, static files, search files, contact, services, about, home

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from pages.views import home
-
+from pages.views import home, about, location, services, contact
 
 urlpatterns = [
     path('', home, name="home"),
+    path('about', about, name='about'),
+    path('location/', location, name='location'),
+    path('services', services, name='services'),
+    path('contact/', contact, name='contact')
 ]
