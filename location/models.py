5f2053b3f61c57271396d374f519f673e24eb35f
Alibouss <bouss58@hotmail.com>
2021-08-18 20:19:38 +0200

    mise en plce du model location

--- location/models.py
+++ location/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Location(models.Model):
+    title = models.CharField(max_length=123)
+    locality = models.CharField(max_length=123)
+    type = models.CharField(max_length=123)
+    category = models.CharField(max_length=123)
+    description = models.CharField(max_length=123)
+    price = models.IntegerField(max_length=123)
+    date_on = models.DateTimeField(max_length=123)
+    date_end = models.DateTimeField(max_length=123)
+    condition = models.CharField(max_length=123)
+    photo = models.ImageField(upload_to='photos')
+    photo_1 = models.ImageField(upload_to='photos')
+    photo_2 = models.ImageField(upload_to='photos')
+    photo_3 = models.ImageField(upload_to='photos')
+    photo_4 = models.ImageField(upload_to='photos')
+
+    def __str__(self):
+        return self.title
---
+++ templates/data.py
@@ -0,0 +1,26 @@
+category = (
+    "House/Maison",
+    "Appartement",
+    "Villa",
+    "Cars/Voitures"
+    "Bikes/Vélos",
+    "Moto",
+    "Air Conditioner",
+    "Pool/Piscine",
+    "Jacuzzi",
+    "Generator/Groupe",
+    "Pc Portable",
+    "Siege Auto",
+    "Alarme",
+)
+
+conditions = {
+    "Neuf",
+    "Trés Propre",
+    "Occasion",
+    "Mauvais état"
+}
+
+types = {
+
+}
